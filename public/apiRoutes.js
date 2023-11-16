6d058debf12ddaafd463ecefe7017dfb45e76327
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-15 19:11:55 -0500

    basic server setup

--- public/apiRoutes.js
+++ public/apiRoutes.js
@@ -1,1 +1,57 @@
-import express from 'express'
+import express from 'express';
+import pg from 'pg';
+import cors from 'cors';
+
+const { Pool } = pg;
+const apiPort = process.env.PORT || 3000;
+const app = express();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        require:true
+    }
+});
+
+//  ------------------------------------------------------------ MIDDLEWARE
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'))
+
+
+//  ------------------------------------------------------------ API ROUTES
+
+
+app.get('/api/user', async(req, res) => {
+    
+    try{
+        const result = await pool.query(
+            `SELECT * FROM users`
+        );
+        res.status(201).send(result.rows)
+    } catch (error){
+        console.log(error)
+        res.status(400).json(error)
+    }
+})
+
+
+//  ------------------------------------------------------------ CATCH ALL ROUTE
+app.use('/', (req, res, next) => {
+    next({message: "The path you are looking for does not exist", status: 404})
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status).json({ error: err })
+})
+
+
+//  ------------------------------------------------------------ LISTENER METHOD
+
+
+app.listen(apiPort, () => {
+    console.log(`api server listening on port ${apiPort}`)
+});
+
+//  ------------------------------------------------------------ UTIL FUNCTIONS
